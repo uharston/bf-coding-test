ce64ff862ed4da92b360405b9f9944f9df250bbe
uharston <uharston@gmail.com>
2021-02-11 11:53:10 -0600

    Add create post route for pdf upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'medical_insurance_plan_importer#home'
+  root 'medical_insurance_plan#home'
+  post '/', to: 'medical_insurance_plan#create', as: "medical_insurance_plans"
+
 end
