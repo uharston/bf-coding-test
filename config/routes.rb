5dd15db24610f70c21657a8d83fac8a73613d388
uharston <uharston@gmail.com>
2021-02-12 09:55:58 -0600

    Add download route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'medical_insurance_plan#home'
   post '/', to: 'medical_insurance_plan#create', as: "medical_insurance_plans"
+  # resources :medical_insurance_plan, only: %i[index new create show]
+  get "/download" , to: 'medical_insurance_plan#download', as: :download
+
 
 end
